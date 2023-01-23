b8b0b34a683b4d95e21cf6c60c60052c9fbcb049
eelli002 <elijah_elliott@hotmail.com>
2023-01-22 16:34:19 -0800

    Functionality Finished

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 
 function App() {
   const [name, setName] = useState("");
+  const [users, setUsers] = useState([]);
 
   const On_Submit_Form = async (e) => {
     e.preventDefault();
     try 
     {
-      const response = await fetch(`http://localhost:3002/users/?name=${name}`)
+      const response = await fetch(`http://localhost:3002/users/?name=${name}`);
+      const parse_response = await response.json();
+      setUsers(parse_response);
     } 
     catch (error) {
       console.error(error.message)
@@ -19,7 +22,7 @@ function App() {
     <>
       <div className='container text-center'>
         <h1 className='my-5'>User List</h1>
-        <form className='d-flex'>
+        <form className='d-flex' onSubmit={On_Submit_Form}>
           <input 
             type='text' 
             name='name' 
@@ -30,6 +33,24 @@ function App() {
           />
           <button className='btn btn-success'>Submit</button>
         </form>
+        <table className='table my-3'>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            { users.map(user => (
+                <tr key={user.id}>
+                  <td>{user.first_name}</td>
+                  <td>{user.last_name}</td>
+                </tr>
+            ))
+            }
+          </tbody>
+        </table>
+        {!users.length && <p>No Results Found</p>}
       </div>
     </>
   );
